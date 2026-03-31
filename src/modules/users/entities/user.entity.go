b0884df5d23571b305f29ec353936f73342f4641
JsCodeDevlopment <jonatasilva118@gmail.com>
2026-03-30 23:22:25 -0300

    feat: add User entity, configure production database connection, and document deployment strategy

--- src/modules/users/entities/user.entity.go
+++ src/modules/users/entities/user.entity.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"-"`
+	Password string `json:"-" gorm:"column:password_hash"`
 	Role     string `json:"role" gorm:"default:'user'"`
 	Avatar   string `json:"avatar"`
 }
