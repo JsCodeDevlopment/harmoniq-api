88a84a7242e073d830282ea3c974b8292a9ee7c1
JsCodeDevlopment <jonatasilva118@gmail.com>
2026-03-30 22:43:40 -0300

    feat: implement database connection configuration and update environment variables and gitignore rules

--- src/config/database.go
+++ src/config/database.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,15 +11,7 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-	sslmode := os.Getenv("DB_SSLMODE")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=America/Sao_Paulo",
-		host, user, password, dbname, port, sslmode)
+	dsn := os.Getenv("DATABASE_URL")
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
